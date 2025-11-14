9b9aed1ec626a3190ec075a6a5bc7f4892b4a966
Ninna Melo <ninna.devsystem@gmail.com>
2025-11-14 14:30:19 -0300

    Create lumi.ts

---
+++ lumi.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@lumi.new/sdk'
+
+export const lumi = createClient({
+  projectId: 'p380045078914162688',
+  apiBaseUrl: 'https://api.lumi.new',
+  authOrigin: 'https://auth.lumi.new',
+})
